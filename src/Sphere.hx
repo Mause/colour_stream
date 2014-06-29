4a28fb7c358e5644a520efb9d3911c0cfb22bc2a
Dominic May <jack.thatch@gmail.com>
2014-06-29 13:26:31 +0800

    Delete old version of Sphere code

--- src/Sphere.hx
+++ src/Sphere.hx
@@ -45,44 +45,4 @@ class Sphere {
             within_radius([0, 0, 0], outer_radius, inner_radius)
         );
     }
-
-    // function points(R : Int, D : Int) : Array<Array<Int>> {
-    //     var all_points = [];
-
-    //     for (part in partitions(R, D)) {             // e.g. 4->[3,1]
-    //         // e.g. [3,1]->[3,1,0] (padding)
-    //         // trace('part in partitions', part);
-
-    //         var num_padding = Math.floor(D - part.length);
-    //         for (arb in 0...num_padding) {
-    //             part.push(0);
-    //         }
-
-    //         // e.g. [1,3,0], [1,0,3], ...
-    //         var perms : Array<Array<Int>> = permutations(part);
-    //         var unique_perms : Array<Array<Int>> = [];
-
-    //         for (perm in perms) {
-    //             if (!(unique_perms.indexOf(perm) >= 0)) {
-    //                 unique_perms.push(perm);
-    //             }
-    //         }
-
-    //         for (perm in perms) {
-    //             // e.g. [1,3,0], [-1,3,0], [1,-3,0], [-...
-    //             // trace('perm in perms', perm);
-
-    //             var filtered_points = [];
-    //             for (x in perm) {
-    //                 // trace('x in perm', x);
-    //                 filtered_points.push(x != 0 ? [-x, x] : [0]);
-    //             }
-
-    //             var new_points = product(filtered_points);
-    //             all_points = all_points.concat(new_points);
-    //         }
-    //     }
-
-    //     return all_points;
-    // }
 }
