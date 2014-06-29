6355807b8dcc297176ae64eefe9a8e5d09cb0635
Dominic May <jack.thatch@gmail.com>
2014-06-30 00:55:01 +0800

    Added used colour determiner
    Choices are;
     * bloom filter based, inaccurate but memory inefficent
     * hashset based, accurate but used just over a gigabyte of memory to
     store the data

--- src/ColourCube.hx
+++ src/ColourCube.hx
@@ -1,3 +1,26 @@
+import de.polygonal.ds.HashSet;
+
+
+interface ColourUsedDeterminer {
+    function colourUsed(col : ColourProxy) : Bool;
+    function consume(col : ColourProxy) : Void;
+}
+
+class BloomFilterCUD implements ColourUsedDeterminer {
+    private var used : BloomFilter;
+    public function new() used = new BloomFilter(32, 50);
+    public function consume(col : ColourProxy) this.used.add(col.toString());
+    public function colourUsed(col : ColourProxy) return this.used.has(col.toString());
+}
+
+class HashSetCUD implements ColourUsedDeterminer {
+    private var used : HashSet<ColourProxy>;
+    public function new() used = new HashSet<ColourProxy>(256 * 256 * 256);
+    public function consume(col : ColourProxy) this.used.set(col);
+    public function colourUsed(col : ColourProxy) return this.used.has(col);
+}
+
+
 class ColourCube {
     public var used : ColourUsedDeterminer;
 
