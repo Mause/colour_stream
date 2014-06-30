9de86fc42550d0701b09ea9dbfb1c678689ab394
Dominic May <jack.thatch@gmail.com>
2014-07-01 00:15:30 +0800

    Clean up useless code

--- src/Seeding.hx
+++ src/Seeding.hx
@@ -2,23 +2,6 @@ class Seeding {
     public static function seed(img : Image) : Array<Position> {
         var seeds = [];
 
-        // var colour = new ColourProxy(Std.random(255), Std.random(255), Std.random(255));
-        // trace('seed; ' + colour);
-
-        // var box_size = 50;
-        // for (x in 1...box_size) {
-        //     for (y in 1...box_size) {
-        //         img.setPixel(new Position(x, y), colour);
-        //     }
-        // }
-
-
-        // var choices = [
-        //     new ColourProxy(255, 0, 0),
-        //     new ColourProxy(0, 255, 0),
-        //     new ColourProxy(0, 0, 255)
-        // ];
-
         for (position in 1...50) {
             var pos = new Position(position, position);
             img.setPixel(pos,
