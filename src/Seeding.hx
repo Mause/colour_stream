98a6bb09c41125d610fc4f7dce3d4f42ce53ee91
Dominic May <jack.thatch@gmail.com>
2014-07-01 00:15:52 +0800

    Added function to display seeding colours

--- src/Seeding.hx
+++ src/Seeding.hx
@@ -13,6 +13,34 @@ class Seeding {
         return seeds;
     }
 
+    public static function showSeeds(seed_colours : Array<Colour>) : Void {
+        trace(seed_colours);
+
+        var seed_display = new flash.display.BitmapData(
+            seed_colours.length * 50, 50,
+            false, 0 // 0x00CCCCCC
+        );
+
+        flash.Lib.current.stage.addChild(
+            new flash.display.Bitmap(seed_display)
+        );
+
+        var y_offset = 0;
+        for (col in seed_colours) {
+            for (x in 0...50) {
+                for (y in 0...50) {
+                    seed_display.setPixel(
+                        x, y_offset + y,
+                        col.asHex()
+                    );
+                }
+
+            }
+
+            y_offset += 50;
+        }
+    }
+
     // static function smudge_out_seeds(img : Image, seeds : Array<Position> ) : Image {
     //     var rseeds = [];
     //     for (seed in seeds) { rseeds.insert(0, seed); }
